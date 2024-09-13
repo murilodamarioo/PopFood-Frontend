cf62015ed91459ea31d2ace5b6e145e88f130f3c
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-12 23:28:41 -0300

    feat: create Input component

---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.DARK_900};
+  border-radius: 5px;
+
+  > input {
+    height: 3.5rem;
+    width: 100%;
+
+    padding: 0.75rem 0.875rem;
+    background-color: transparent;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    border: 0;
+
+    &:placeholder {
+      color: ${({ theme }) => theme.COLORS.LIGHT_500} 
+    }
+  }
+
+  > svg {
+    margin-left: 1rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_500}
+  }
+`

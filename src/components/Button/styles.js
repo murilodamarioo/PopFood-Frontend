cc97cf640153574cf062b4ac30c02116c844ce84
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-12 22:01:49 -0300

    feat: create Button Component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  width: 100%;
+  height: 3.5rem;
+  background-color: ${({ theme, disabled }) => disabled ? theme.COLORS.TOMATO_400 : theme.COLORS.TOMATO_100};  
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+  border: 0;
+  border-radius: 5px;
+
+  padding: 0.75rem 2rem;
+
+  &:hover {
+    background-color: ${({ theme, disabled }) => disabled ? theme.COLORS.TOMATO_400 : theme.COLORS.TOMATO_200};
+    cursor: ${({ disabled }) => disabled ? 'not-allowed' : 'pointer'};
+  }
+`

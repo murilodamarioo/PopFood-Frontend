3c74c304767f54446c6657ded6c0f24078622f7c
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-15 20:46:37 -0300

    refactor: center and space between icon and text

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 export const Container = styled.button`
   width: 100%;
   height: 3.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
   background-color: ${({ theme, disabled }) => disabled ? theme.COLORS.TOMATO_400 : theme.COLORS.TOMATO_100};  
   color: ${({ theme }) => theme.COLORS.LIGHT_100};
 

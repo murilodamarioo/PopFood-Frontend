0c6a13980f140ecf01481645a580056def977120
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-15 18:27:54 -0300

    feat: implement handleSignUp function

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,11 +6,43 @@ import { Input } from '../../components/Input'
 import { FaUser, FaLock } from 'react-icons/fa'
 import { MdEmail} from 'react-icons/md'
 
+import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { api } from '../../services/api'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSignUp() {
+    api.post('/users', { name, email, password })
+    .then(() => {
+      toast.success('Conta criada com sucesso!', {
+        position: "top-center",
+        theme: "dark"
+      })
+    })
+    .catch(error => {
+      if (error.message) {
+        toast.error(error.response.data.message, {
+          position: "top-center",
+          theme: "dark"
+        })
+      } else {
+        toast.error('Não foi possivel criar a conta', {
+          position: "top-center",
+          theme: "dark"
+        })
+      }
+    })
+  }
+
   return (
     <Container>
       <main>
+        <ToastContainer />
         <Logo>
           <svg width="39" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#065E7C" />
@@ -26,6 +58,7 @@ export function SignUp() {
             icon={FaUser}
             type="text"
             title="Seu nome"
+            onChange={event => setName(event.target.value)}
           />
 
           <Input
@@ -33,6 +66,7 @@ export function SignUp() {
             icon={MdEmail}
             type="email"
             title="Email"
+            onChange={event => setEmail(event.target.value)}
           />
 
           <Input
@@ -40,9 +74,10 @@ export function SignUp() {
             icon={FaLock}
             type="password"
             title="Senha"
+            onChange={event => setPassword(event.target.value)}
           />
 
-          <Button title="Criar conta"/>
+          <Button title="Criar conta" onClick={handleSignUp}/>
           <a href="#">Já tenho uma conta</a>
 
         </Form>

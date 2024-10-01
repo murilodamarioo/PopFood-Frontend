e520bf649b19a06f96723671c2ec2346eb8fd8a5
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-30 21:25:59 -0300

    refactor: change width to auto

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
-  width: 100%;
+  width: auto;
   height: 3.5rem;
 
   display: flex;
@@ -16,6 +16,12 @@ export const Container = styled.button`
   border-radius: 5px;
 
   padding: 0.75rem 2rem;
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    flex-shrink: 0;
+  }
 
   &:hover {
     background-color: ${({ theme, disabled }) => disabled ? theme.COLORS.TOMATO_400 : theme.COLORS.TOMATO_200};
